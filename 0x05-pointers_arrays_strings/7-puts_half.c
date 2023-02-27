786ae20282b3525e8b615f79012c92433597e440
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-02-27 09:14:06 -0800

    Update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - returns the length of a string
+ * @str: string
+ * Return: void
+ */
+int puts_half(char *str)
+{
+int len, x, i;
+
+len = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else if (len % 2)
+{
+for (x = (len - 1) / 2; x < len - 1; x++)
+{
+_putchar(str[x + 1]);
+}
+}
+_putchar('\n');
+}
