b1174548b160d9a0a486abeac95532fc740170c7
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-05-09 00:58:22 -0700

    Program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,14 +23,12 @@ void error_file(int file_from, int file_to, char *argv[])
 		exit(99);
 	}
 }
-
 /**
  * main - Fuction that check the code.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -43,21 +41,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
-
 	nchars = 1024;
 	while (nchars == 1024)
-
+	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
-
+	}
 
 	err_close = close(file_from);
 	if (err_close == -1)
@@ -65,12 +61,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 
+	{
 	err_close = close(file_to);
 	if (err_close == -1)
-
-	printf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-	exit(100);
-	{
-	return (0);
 	}
+
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
