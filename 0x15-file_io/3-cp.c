aae918a97e0fcca639e77dbbd27e5f4a3e7a7876
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-05-09 01:04:17 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,12 +23,14 @@ void error_file(int file_from, int file_to, char *argv[])
 		exit(99);
 	}
 }
+
 /**
  * main - Fuction that check the code.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -36,7 +38,6 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
@@ -44,6 +45,7 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
+
 	nchars = 1024;
 	while (nchars == 1024)
 	{
@@ -54,18 +56,14 @@ int main(int argc, char *argv[])
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
-
 	err_close = close(file_from);
 	if (err_close == -1)
-
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
-
 	{
 	err_close = close(file_to);
 	if (err_close == -1)
 	}
-
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
